4eabd31108fd49d630d00f42e55665530d5d40c0
THORinHOOD <ihateyouthor@gmail.com>
2017-12-29 21:26:06 +0300

    Тестовый коммит Дата : 29.12.2017

--- app/src/main/java/catwithbowtie/chatchat/MainActivity.java
+++ app/src/main/java/catwithbowtie/chatchat/MainActivity.java
@@ -76,7 +76,7 @@ public class MainActivity extends AppCompatActivity
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-
+//kek
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
